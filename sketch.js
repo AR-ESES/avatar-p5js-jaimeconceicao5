95f560029bba8e85acd0a5b3f27d768be4c4b53d
Jaime Conceição <jaimeconceicao@MBP-de-Jaime.lan>
2024-03-28 22:44:43 +0000

    avatar

--- sketch.js
+++ sketch.js
@@ -1,11 +1,31 @@
 function setup() {
-  createCanvas(1280, 1024);
-  background(153);
-  stroke(0);
-  strokeWeight(2);
-  fill(237, 28, 36);
-  circle(30, 30, 40);
-  line(30, 30, 1280 / 2, 1024 / 2);
+  createCanvas(1920, 1080);
 }
 
-function draw() {}
+function draw() {
+  background(20,400,30);
+  fill (5,130,103)
+  stroke(4)
+  rect(1920/2,1080/2,40,80)
+  fill(179,146,131); 
+  stroke(0);
+  rect(925,360,70,70);
+  fill (5,130,103)
+  stroke(4)
+  rect(920,540,40,80)
+  fill (777,53,10)
+  stroke(4)
+  rect(920,620,40,10)
+  fill (777,53,10)
+  stroke(4)
+  rect(960,620,40,10)
+  fill (255,25,25)
+  stroke(4)
+  rect(920,430,80,110)
+  fill (255,25,25)
+  stroke(4)
+  rect(890,430,30,90)
+  fill (255,25,25)
+  stroke(4)
+  rect(1000,430,30,90)
+}
